326ff9c42aaef27a57e4d71fe8973c3b69c713e2
Terradclarke <terranie67@gmail.com>
2020-11-19 19:22:22 -0600

    commit tree 1733208b2911b4071ae07e24e8fc3103f30af365

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => console.log(json));
 }
 
 function renderBooks(books) {
@@ -13,4 +15,5 @@ function renderBooks(books) {
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
+  renderBooks()
 })
